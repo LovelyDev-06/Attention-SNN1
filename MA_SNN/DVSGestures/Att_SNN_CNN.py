765c83437fc2c78e278260dc8d55dda03ecc86be
LovelyDev-06 <258448137+LovelyDev-06@users.noreply.github.com>
2026-09-21 23:46:09 +0530

    Update Att_SNN_CNN.py

--- MA_SNN/DVSGestures/Att_SNN_CNN.py
+++ MA_SNN/DVSGestures/Att_SNN_CNN.py
@@ -12,7 +12,7 @@ sys.path.append(rootPath)
 from DVSGestures.CNN import Config
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "4,"
-config = configs()
+config = config()
 config.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(config.device)
 
