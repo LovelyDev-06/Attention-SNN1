9ae60e1494acee9c52f47db4f2fb94276086e006
LovelyDev-06 <258448137+LovelyDev-06@users.noreply.github.com>
2026-09-21 22:09:41 +0530

    Fix string encoding issues in Att_SNN_CNN.py

--- MA_SNN/DVSGestures/Att_SNN_CNN.py
+++ MA_SNN/DVSGestures/Att_SNN_CNN.py
@@ -2,7 +2,7 @@ import os
 
 import sys
 
-sys.path.append(os.path.dirname("__file__"))
+sys.path.append(os.path.dirname(__file__))
 from DVSGestures.CNN import Att_SNN
 
 rootPath = os.path.abspath(os.path.dirname(__file__))
