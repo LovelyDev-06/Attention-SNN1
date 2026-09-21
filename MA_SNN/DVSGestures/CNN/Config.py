af29271d0a0eb8ab708b945a04ba144fc829f741
LovelyDev-06 <258448137+LovelyDev-06@users.noreply.github.com>
2026-09-21 23:57:00 +0530

    Reduce batch size for training and testing

--- MA_SNN/DVSGestures/CNN/Config.py
+++ MA_SNN/DVSGestures/CNN/Config.py
@@ -15,8 +15,8 @@ class configs(object):
         self.onlyTest = True
         self.pretrained_path = os.path.dirname(os.path.abspath(__file__)) + os.sep + "Results" + os.sep + "CSA_SNN(CNN)-DVS-Gesture_dt=25ms_T=40.t7"
 
-        self.batch_size = 128
-        self.batch_size_test = 128
+        self.batch_size = 32
+        self.batch_size_test = 32
 
         # None  'kaiming'  'xavier'
         self.init_method = None
